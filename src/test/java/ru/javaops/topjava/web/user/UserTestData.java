e3c13a11c4589ae5c113f90652788cc8d1fa70c2
1strebel <firstrebel@ya.ru>
2023-12-25 19:59:44 +0300

    Fix forwarding

--- src/test/java/ru/javaops/topjava/web/user/UserTestData.java
+++ src/test/java/ru/javaops/topjava/web/user/UserTestData.java
@@ -14,15 +14,17 @@ import static ru.javaops.topjava.web.meal.MealTestData.*;
 
 public class UserTestData {
     public static final MatcherFactory.Matcher<User> USER_MATCHER = MatcherFactory.usingIgnoringFieldsComparator(User.class, "registered", "meals", "password");
-    public static final User user = new User(USER_ID, "User", USER_MAIL, "password", 2005, Role.USER);
 
     public static final int USER_ID = 1;
     public static final int ADMIN_ID = 2;
     public static final int GUEST_ID = 3;
     public static final int NOT_FOUND = 100;
+
     public static final String USER_MAIL = "user@yandex.ru";
     public static final String ADMIN_MAIL = "admin@gmail.com";
     public static final String GUEST_MAIL = "guest@gmail.com";
+    public static final User user = new User(USER_ID, "User", USER_MAIL, "password", 2005, Role.USER);
+
     public static final User admin = new User(ADMIN_ID, "Admin", ADMIN_MAIL, "admin", 1900, Role.ADMIN, Role.USER);
     public static final User guest = new User(GUEST_ID, "Guest", GUEST_MAIL, "guest", 2000);
     public static MatcherFactory.Matcher<User> USER_WITH_MEALS_MATCHER =
